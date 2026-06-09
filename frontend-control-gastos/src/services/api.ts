2f37d21de33823ae2409472f3bd782efac104b46
Yamil Luis Villca <yamilluisvillca5b@gmail.com>
2026-06-09 15:02:31 -0600

    Actualizar URL backend Render

--- frontend-control-gastos/src/services/api.ts
+++ frontend-control-gastos/src/services/api.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://financiacompleto-3.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem('token');
 
   if (token) {
