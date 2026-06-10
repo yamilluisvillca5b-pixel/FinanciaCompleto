3a00040842207f1cfc269c9eb9e1fdfe1dbc3e0a
Yamil Luis Villca <yamilluisvillca5b@gmail.com>
2026-06-09 20:59:53 -0600

    quitar captcha temporal

--- frontend-control-gastos/src/pages/Login.tsx
+++ frontend-control-gastos/src/pages/Login.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
-import ReCAPTCHA from 'react-google-recaptcha';
+// IMPORT COMENTADO TEMPORALMENTE
+// import ReCAPTCHA from 'react-google-recaptcha';
 
 function Login() {
   const navigate = useNavigate();
 
   const [nombre, setNombre] = useState('');
   const [password, setPassword] = useState('');
-  const [captcha, setCaptcha] = useState<string | null>(null);
+  
+  // ESTADO COMENTADO TEMPORALMENTE
+  // const [captcha, setCaptcha] = useState<string | null>(null);
 
   const login = async () => {
-    if (!captcha) {
-      alert('Complete el CAPTCHA');
-      return;
-    }
+    // VALIDACIÓN COMENTADA TEMPORALMENTE
+    // if (!captcha) {
+    //   alert('Complete el CAPTCHA');
+    //   return;
+    // }
 
     try {
       const response = await api.post('/auth/login', {
@@ -22,20 +26,9 @@ function Login() {
         password,
       });
 
-      localStorage.setItem(
-        'token',
-        response.data.access_token
-      );
-
-      localStorage.setItem(
-        'usuarioId',
-        response.data.usuario.id
-      );
-
-      localStorage.setItem(
-        'tipoUsuario',
-        response.data.usuario.tipo_usuario
-      );
+      localStorage.setItem('token', response.data.access_token);
+      localStorage.setItem('usuarioId', response.data.usuario.id);
+      localStorage.setItem('tipoUsuario', response.data.usuario.tipo_usuario);
 
       navigate('/dashboard');
     } catch (error: any) {
@@ -54,21 +47,18 @@ function Login() {
           type="text"
           placeholder="Usuario"
           value={nombre}
-          onChange={(e) =>
-            setNombre(e.target.value)
-          }
+          onChange={(e) => setNombre(e.target.value)}
         />
 
         <input
           type="password"
           placeholder="Contraseña"
           value={password}
-          onChange={(e) =>
-            setPassword(e.target.value)
-          }
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <div
+        {/* COMPONENTE RECAPTCHA COMENTADO TEMPORALMENTE */}
+        {/* <div
           style={{
             marginTop: '15px',
             marginBottom: '15px',
@@ -78,11 +68,10 @@ function Login() {
         >
           <ReCAPTCHA
             sitekey="6Ld4zhUtAAAAAKzwz2EvUZvvrdBUDrM93kHYUSyu"
-            onChange={(value) =>
-              setCaptcha(value)
-            }
+            onChange={(value) => setCaptcha(value)}
           />
-        </div>
+        </div> 
+        */}
 
         <button onClick={login}>
           Iniciar Sesión
